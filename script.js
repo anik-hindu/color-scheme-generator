144a1f3551537a9e0af387b65674cc655574883c
Anik Saha <aniksahaearnbd@gmail.com>
2026-05-06 08:51:07 +0600

    Test colorSchemeAPI with test data

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+const colorApiScheme = "https://www.thecolorapi.com/scheme";
+const colorMode = "monochrome";
+const hexValue = "0047AB";
+
+fetch(`${colorApiScheme}?hex=${hexValue}&mode=${colorMode}&count=5`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
